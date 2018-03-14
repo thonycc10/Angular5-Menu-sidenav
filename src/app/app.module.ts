8340f53a9400f1af1cd5dfa635b18dccc0d65789
Anthony Carrasco <anthony.carrasco@attlaris.pe>
2018-03-14 11:54:59 -0500

    Ejemplo menu y sidenav con material angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatMenuModule, MatIconModule, MatCardModule, MatSidenavModule} from '@angular/material';
+import { SlidernavdemoComponent } from './slidernavdemo/slidernavdemo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SlidernavdemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatMenuModule, MatIconModule, MatCardModule, MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
